7794ee7adab0981fcf1eb0437b10ad7accf4c7c6
vaishakh k <38469664+vyshakkiradathil@users.noreply.github.com>
2024-01-24 10:23:08 +0530

    updated tf

--- template/modules/azure/public_vm/main.tf
+++ template/modules/azure/public_vm/main.tf
@@ -1,4 +1,27 @@
-resource "azurerm_resource_group" "example" {
-  name     = var.name
-  location = var.region
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.47.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "rg_name" {
+  type = string
+  description = "Resource group Name"
+}
+
+variable "rg_location" {
+  type = string
+  description = "Location to create RG in"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_location
 }
