d1f166f5f1c1f825ae73203c7e2870b1b2ed4157
vaishakh k <38469664+vyshakkiradathil@users.noreply.github.com>
2024-01-30 11:01:23 +0530

    created tf module

--- template/main.tf
+++ template/main.tf
@@ -1,106 +1,19 @@
-# Specify the provider and access details
-provider "aws" {
-  region = var.aws_region
-  access_key = var.access_key
-  secret_key = var.secret_key
-  token = var.token == "" ? null : var.token
-}
-
-# Create a VPC
-resource "aws_vpc" "example_vpc" {
-  cidr_block = "10.10.0.0/16"
-  enable_dns_hostnames = true
-}
-
-resource "aws_internet_gateway" "example_gateway" {
-  vpc_id = aws_vpc.example_vpc.id
-}
-
-# Create a subnet to launch our instances into
-resource "aws_subnet" "example_subnet" {
-  vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.10.4.0/24"
-  map_public_ip_on_launch = false
-  availability_zone = var.aws_zone
-}
-
-resource "aws_route" "example_route" {
-  route_table_id = aws_vpc.example_vpc.default_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.example_gateway.id
-}
-
-# Security group for our application.
-resource "aws_security_group" "example_security_group" {
-  name_prefix = "example_security_group-"
-  description = "Security group for example application"
-  vpc_id      = aws_vpc.example_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 9990
-    to_port     = 9990
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 2375
-    to_port     = 2375
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 4566
-    to_port     = 4566
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 4571
-    to_port     = 4571
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  lifecycle {
-    create_before_destroy = true
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.47.0"
+    }
   }
 }
 
-output "subnet_id" {
-  value = aws_subnet.example_subnet.id
+provider "azurerm" {
+  features {}
 }
 
-output "group_ids" {
-  value = [aws_security_group.example_security_group.id]
+module "resource_group" {
+  source  = "./modules/azure_rg"
+  
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
---
+++ template/modules/azure_rg/variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+}
+
+variable "location" {
+  description = "The location (region) of the resource group"
+}
